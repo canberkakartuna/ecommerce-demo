0d35f75fba9b402ce986f578a9935242abf27023
Canberk Akartuna <canberkakartuna@hotmail.com>
2021-05-02 15:00:18 +0300

    ecommerce proj init

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a demo React project from [zerotomastery.io's Udemy Course](https://www.udemy.com/course/complete-react-developer-zero-to-mastery/)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import HomePage from './homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
